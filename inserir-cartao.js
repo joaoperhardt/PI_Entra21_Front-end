39b04d4701b0b5a769bcac95d6c676734b4e50a3
João Pedro Erhardt <joaopedro.erhardt@gmail.com>
2023-12-02 00:48:04 -0300

    add input conditions

--- inserir-cartao.js
+++ inserir-cartao.js
@@ -10,5 +10,54 @@ $(() => {
         }
 
         console.log(valuesJson);
+
+        if (!valuesJson.cpf) {
+            InvalidateInput("CPF não informado", "#user-cpf")
+            return;
+        }
+        else if (valuesJson.cpf.length !== 11) {
+            InvalidateInput("CPF inválido", "#user-cpf")
+            return;
+        }
+        $("#user-cpf").removeClass("invalid-label")
+
+        if (!valuesJson.cardName) {
+            InvalidateInput("Nome do cartão não informado", "#card-name")
+            return
+        }
+        $("#card-name").removeClass("invalid-label")
+
+        if (!valuesJson.cardNumber) {
+            InvalidateInput("Número do cartão não informado", "#card-number")
+            return
+        }
+        else if (valuesJson.cardNumber.length !== 12) {
+            InvalidateInput("Número do cartão inválido", "#card-number")
+            return
+        }
+        $("#card-number").removeClass("invalid-label")
+
+        if (!valuesJson.cardCvv) {
+            InvalidateInput("CVV do cartão não informado", "#card-cvv")
+            return
+        }
+        else if (valuesJson.cardCvv.length !== 3) {
+            InvalidateInput("CVV inválido", "#card-cvv")
+        }
+        $("#card-cvv").removeClass("invalid-label")
+
+        if (!valuesJson.cardExpiringYear) {
+            InvalidateInput("Ano de vencimento do cartão não informado", "#expiring-date-year")
+            return
+        }
+        else if (valuesJson.cardExpiringYear.length !== 4 || parseInt(valuesJson.cardExpiringYear) < 2022) {
+            InvalidateInput("Ano de vencimento inválido", "#expiring-date-year")
+        }
+        $("#expiring-date-year").removeClass("invalid-label")
     })
-}) 
+})
+
+function InvalidateInput(msg, id) {
+    alert(msg)
+    $(id).addClass("invalid-label")
+}
