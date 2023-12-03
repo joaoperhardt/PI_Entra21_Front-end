a674c67567165a0bb4c07b32c15306b04fc285f4
João Pedro Erhardt <joaopedro.erhardt@gmail.com>
2023-12-03 20:35:46 -0300

    add add-to-cart-button click event

--- produto.js
+++ produto.js
@@ -1,3 +1,14 @@
 $(() => {
-    
+    $(".add-to-cart-button").click(() => {
+        const productJson = {
+            name: $(".product-name")[0].innerHTML,
+            price: $(".product-price")[0].innerHTML.replace(",", "."),
+            discountPercentage: $(".discount-tag")[0].innerHTML,
+            quantity: $(".product-quantity-setter")[0].value,
+            freight: $(".product-freight")[0].innerHTML.replace(",", "."),
+            volunteers: String(parseInt($(".product-volunteers")[0].innerHTML) + 1),
+            volunteersNeeded: $(".product-volunteers-needed")[0].innerHTML 
+        }
+        console.log(productJson)
+    })
 })
