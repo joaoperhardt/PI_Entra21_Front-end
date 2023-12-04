93e9b6b123b27edb7745d2f6718b8f408d72827b
João Pedro Erhardt <joaopedro.erhardt@gmail.com>
2023-12-04 18:55:54 -0300

    add buy-button on click event

--- produto.js
+++ produto.js
@@ -2,13 +2,30 @@ $(() => {
     $(".add-to-cart-button").click(() => {
         const productJson = {
             name: $(".product-name")[0].innerHTML,
+            originalPrice: parseFloat($(".product-original-price")[0].innerHTML.replace(",", ".")),
+            price: parseFloat($(".product-price")[0].innerHTML.replace(",", ".")),
+            discountPercentage: $(".discount-tag")[0].innerHTML,
+            quantity: parseInt($(".product-quantity-setter")[0].value),
+            freight: parseFloat($(".product-freight")[0].innerHTML.replace(",", ".")),
+            totalPrice: (this.price * this.quantity) + this.freight,
+            volunteers: parseInt($(".product-volunteers")[0].innerHTML) + 1,
+            volunteersNeeded: $(".product-volunteers-needed")[0].innerHTML
+        }
+        console.log(productJson)
+    })
+
+    $(".buy-button").click(() => {
+        const productJson = {
+            name: $(".product-name")[0].innerHTML,
+            originalPrice: $(".product-original-price")[0].innerHTML.replace(",", "."),
             price: $(".product-price")[0].innerHTML.replace(",", "."),
             discountPercentage: $(".discount-tag")[0].innerHTML,
             quantity: $(".product-quantity-setter")[0].value,
             freight: $(".product-freight")[0].innerHTML.replace(",", "."),
             volunteers: String(parseInt($(".product-volunteers")[0].innerHTML) + 1),
-            volunteersNeeded: $(".product-volunteers-needed")[0].innerHTML 
+            volunteersNeeded: $(".product-volunteers-needed")[0].innerHTML
         }
         console.log(productJson)
     })
 })
+
